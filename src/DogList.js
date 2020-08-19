6f2628b075e947cf03339e9d7c4e16978e7ae860
BenoitGaudieri <benalek@gmail.com>
2020-08-20 00:33:28 +0200

    style

--- src/DogList.js
+++ src/DogList.js
@@ -6,13 +6,18 @@ export default class DogList extends Component {
     render() {
         return (
             <div className="DogList">
-                <h1 className="display-1 text-center">Dogs</h1>
+                <h1 className="display-1 text-center mt-3 mb-5">Dogs</h1>
                 <div className="row">
                     {this.props.dogs.map((d) => (
                         <div className="Dog col-lg-4 text-center" key={d.name}>
                             <img src={d.src} alt={d.name} />
                             <h3>
-                                <Link to={`/dogs/${d.name}`}>{d.name}</Link>
+                                <Link
+                                    className="underline"
+                                    to={`/dogs/${d.name}`}
+                                >
+                                    {d.name}
+                                </Link>
                             </h3>
                         </div>
                     ))}
